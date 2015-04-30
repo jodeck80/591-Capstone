274e87d4fe0b309c8f3d065397c44a0144c7c3c1
jodeck80 <jodeck80@gmail.com>
2015-04-30 18:35:58 -0400

    Update capstone.R

--- capstone.R
+++ capstone.R
@@ -6,79 +6,251 @@ library(maps)
 library(RStorm)
 load("my_oauth.Rdata")
 
-#Vectors of nicknames for teams
-#Doing NBA teams in playoffs
-#constant vector of types, 4 buzz words for each team
-#namesCavs <- c("cleveland", "cavaliers", "cavs", "Lebron")
-#namesHawks <- c("atlanta", "hawks", "horford", "atl")
-#namesBulls <- c("chicago", "bulls", "butler", "noah")
-#namesWizards <- c("washington", "wizards", "pierce", "Lebron")
-#namesWarriors <- c("golden state", "warriors", "curry", "steph")
-#namesRockets <- c("houston", "rockets", "harden", "howard")
-#namesGrizzlies <- c("memphis", "grizzlies", "randolph", "gasol")
-#namesSpurs <- c("san antonio", "spurs", "duncan", "ginobili")
+# Color blind friendly colors:
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-#run these prior to starting loop
-#handshake for twitter credentials
-my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+#1 minute tweet duration
+TWEET_DURATION <- 60
 
-#define globally, used in many functions
-tweets.running <- 0
+#number of times to iterate process
+ITERATIONS <- 30
+
+MLBTerms <- c("MLB","yankees", "red sox", "orioles", "rays", "blue jays",
+              "mets", "braves", "marlins", "phillies", "nationals",
+              "tigers", "twins", "indians", "royals", "white sox",
+              "cubs", "cardinals", "reds", "pirates", "brewers",
+              "mariners", "a's", "astros", "angels", "rangers",
+              "dodgers", "giants", "diamondbacks", "padres", "rockies")
+NBATerms <- c("NBA","hawks", "cavaliers", "bulls", "raptors", "wizards",
+              "bucks", "celtics", "nets", "pacers", "heat",
+              "hornets", "pistons", "magic", "76ers", "knicks",
+              "warriors", "rockets", "clippers", "spurs", "trail blazers",
+              "grizzlies", "mavericks", "pelicans", "thunder", "suns",
+              "jazz", "nuggets", "kings", "lakers", "timberwolves")
+
+NFLTerms <- c("NFL","eagles", "giants", "cowboys", "redskins", "bills",
+              "patriots", "dolphins", "jets", "bears", "lions",
+              "packers", "vikings", "browns", "bengals", "steelers",
+              "ravens", "panthers", "saints", "buccaneers", "falcons",
+              "titans", "colts", "jaguars", "texans", "49ers",
+              "cardinals", "seahawks", "rams", "broncos", "chargers", "raiders", "chiefs")
+
+NHLTerms <- c("NHL","rangers", "canadiens", "lightning", "capitals", "islanders",
+              "red wings", "senators", "penguins", "bruins", "panthers",
+              "blue jackets", "flyers", "devils", "hurricanes", "maple leafs",
+              "sabres", "ducks", "blues", "predators", "blackhawks",
+              "canucks", "wild", "jets", "flames", "kings",
+              "stars", "avalanche", "sharks", "oilers", "coyotes")
+
 
 #function to remove tweets having both the words love and hate
-filterTweets<-function(a)
+filterTweetsMLB<-function(a)
 {
   # check if text doesn't have both love and hate in it
-  if((length(grep("sox",a,ignore.case=TRUE))>0) | 
-       (length(grep("yankees",a,ignore.case=TRUE))>0) | 
-       (length(grep("tigers",a,ignore.case=TRUE))>0) | 
-       (length(grep("dodgers",a,ignore.case=TRUE))>0) | 
-       (length(grep("marlins",a,ignore.case=TRUE))>0) |
-       (length(grep("cardinals",a,ignore.case=TRUE))>0) |
-       (length(grep("MLB",a,ignore.case=TRUE))>0))
+  if((length(grep(MLBTerms[1],a,ignore.case=TRUE))>0) | 
+       (length(grep(MLBTerms[2],a,ignore.case=TRUE))>0) | 
+       (length(grep(MLBTerms[3],a,ignore.case=TRUE))>0) | 
+       (length(grep(MLBTerms[4],a,ignore.case=TRUE))>0) | 
+       (length(grep(MLBTerms[5],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[6],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[7],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[8],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[9],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[10],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[11],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[12],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[13],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[14],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[15],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[16],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[17],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[18],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[19],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[20],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[21],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[22],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[23],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[24],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[25],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[26],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[27],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[28],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[29],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[30],a,ignore.case=TRUE))>0) |
+       (length(grep(MLBTerms[31],a,ignore.case=TRUE))>0))
   {
-    #set league to 1
-    a$league = 1
-    
-    #Hashmap to keep approximation of league count
-    words <- GetHash("wordcount")
-    if("MLB" %in% words$word){
-      words[words$word == "MLB",]$count <- words[words$word == "MLB",]$count + 1
-    }else
+    if(a$league != 0)
     {
-      words <- rbind(words, data.frame(word = "MLB", count = 1))
+      #another league was also mentioned, will remove tweet
+      a$league = 5
     }
-    # Store the hashmap
-    SetHash("wordcount", words)    
+    else
+    {
+      #set league to 1 if league wasnt previously set
+      a$league = "MLB"
+    }
   }
   data.frame(a)
 }
+
 
 #function to remove tweets having both the words love and hate
 filterTweetsNBA<-function(a)
 {
   # check if text doesn't have both love and hate in it
-  if((length(grep("cavs",a,ignore.case=TRUE))>0) | 
-       (length(grep("spurs",a,ignore.case=TRUE))>0) | 
-       (length(grep("NBA",a,ignore.case=TRUE))>0) | 
-       (length(grep("warriors",a,ignore.case=TRUE))>0) | 
-       (length(grep("clippers",a,ignore.case=TRUE))>0) | 
-       (length(grep("hawks",a,ignore.case=TRUE))>0))
+  if((length(grep(NBATerms[1],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[2],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[3],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[4],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[5],a,ignore.case=TRUE))>0) |
+       (length(grep(NBATerms[6],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[7],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[8],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[9],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[10],a,ignore.case=TRUE))>0) |
+       (length(grep(NBATerms[11],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[12],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[13],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[14],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[15],a,ignore.case=TRUE))>0) |
+       (length(grep(NBATerms[16],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[17],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[18],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[19],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[20],a,ignore.case=TRUE))>0) |
+       (length(grep(NBATerms[21],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[22],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[23],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[24],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[25],a,ignore.case=TRUE))>0) |
+       (length(grep(NBATerms[26],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[27],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[28],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[29],a,ignore.case=TRUE))>0) | 
+       (length(grep(NBATerms[30],a,ignore.case=TRUE))>0) )
   {
-    # return it as data frame
-    #data.frame(a)
-    a$league = 2
+    if(a$league != 0)
+    {
+      #another league was also mentioned, will remove tweet
+      a$league = 5
+    }
+    else
+    {
+      #set league to 2 if league wasnt previously set
+      a$league = "NBA"
+    }
   }
-
   data.frame(a)
 }
 
 
-updateTweets<-function(a){
+#function to remove tweets having both the words love and hate
+filterTweetsNFL<-function(a)
+{
+  # check if text doesn't have both love and hate in it
+  if((length(grep(NFLTerms[1],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[2],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[3],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[4],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[5],a,ignore.case=TRUE))>0) |
+       (length(grep(NFLTerms[6],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[7],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[8],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[9],a,ignore.case=TRUE))>0) |
+       (length(grep(NFLTerms[10],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[11],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[12],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[13],a,ignore.case=TRUE))>0) |
+       (length(grep(NFLTerms[14],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[15],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[16],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[17],a,ignore.case=TRUE))>0) |
+       (length(grep(NFLTerms[18],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[19],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[20],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[21],a,ignore.case=TRUE))>0) |
+       (length(grep(NFLTerms[22],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[23],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[24],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[25],a,ignore.case=TRUE))>0) |
+       (length(grep(NFLTerms[26],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[27],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[28],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[29],a,ignore.case=TRUE))>0) |
+       (length(grep(NFLTerms[30],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[31],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[32],a,ignore.case=TRUE))>0) | 
+       (length(grep(NFLTerms[33],a,ignore.case=TRUE))>0) )
+  {
+    if(a$league != 0)
+    {
+      #another league was also mentioned, will remove tweet
+      a$league = 5
+    }
+    else
+    {
+      #set league to 3 if league wasnt previously set
+      a$league = "NFL"
+    }
+  }
+  data.frame(a)
+}
 
-#TODO, maybe update to minute
+#function to remove tweets having both the words love and hate
+filterTweetsNHL<-function(a)
+{
+  # check if text doesn't have both love and hate in it
+  if((length(grep(NHLTerms[1],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[2],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[3],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[4],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[5],a,ignore.case=TRUE))>0) |
+       (length(grep(NHLTerms[6],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[7],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[8],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[9],a,ignore.case=TRUE))>0) |
+       (length(grep(NHLTerms[10],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[11],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[12],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[13],a,ignore.case=TRUE))>0) |
+       (length(grep(NHLTerms[14],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[15],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[16],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[17],a,ignore.case=TRUE))>0) |
+       (length(grep(NHLTerms[18],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[19],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[20],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[21],a,ignore.case=TRUE))>0) |
+       (length(grep(NHLTerms[22],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[23],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[24],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[25],a,ignore.case=TRUE))>0) |
+       (length(grep(NHLTerms[26],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[27],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[28],a,ignore.case=TRUE))>0) | 
+       (length(grep(NHLTerms[29],a,ignore.case=TRUE))>0) |
+       (length(grep(NHLTerms[30],a,ignore.case=TRUE))>0) |
+       (length(grep(NHLTerms[31],a,ignore.case=TRUE))>0))
+  {
+    if(a$league != 0)
+    {
+      #another league was also mentioned, will remove tweet
+      a$league = 5
+    }
+    else
+    {
+      #set league to 4 if league wasnt previously set
+      a$league = "NHL"
+    }  
+  }
+  data.frame(a)
+}
+
+#Capture new tweets and 
+updateTweets<-function(a, firstTime){
+
 #stream tweets only in US locations
-filterStream("tweetsUS.json", locations = c(-125, 25, -66, 50), timeout = 20, oauth = my_oauth)
+filterStream("tweetsUS.json", locations = c(-125, 25, -66, 50), timeout = TWEET_DURATION, oauth = my_oauth)
   
 #parse tweets from .json file
 tweets.df <- parseTweets("tweetsUS.json", verbose = FALSE)
@@ -90,36 +262,33 @@ file.remove("tweetsUS.json")
 tweets.filter <- tweets.df
 tweets.filter$league = 0
 
-# get only tweets if they satisfy filter
-tweets.filter<-ddply(tweets.df,.(text),filterTweets)
-
-# get only tweets if they satisfy filter
+# push tweets through all league filters
+tweets.filter<-ddply(tweets.filter,.(text),filterTweetsMLB)
 tweets.filter<-ddply(tweets.filter,.(text),filterTweetsNBA)
+tweets.filter<-ddply(tweets.filter,.(text),filterTweetsNFL)
+tweets.filter<-ddply(tweets.filter,.(text),filterTweetsNHL)
 
-#Only look at rows where at least one league was selected
-tweets.filter <- subset(tweets.filter, tweets.filter$league!=0 )
+#Only look at rows where one league was selected
+tweets.filter <- subset(tweets.filter, tweets.filter$league == "MLB" | 
+                          tweets.filter$league == "NBA" |
+                          tweets.filter$league == "NFL" |
+                          tweets.filter$league == "NHL")
 
-#append new filtered tweets to old
-tweets.running <- rbind(tweets.running, tweets.filter)
+#if first time, cant bind yet
+if(firstTime)
+{
+  tweets.running <- tweets.filter
+}
+else
+{
+  #append new filtered tweets to old
+  tweets.running <- rbind(a, tweets.filter)
+}
 
 return (tweets.running)
 }
 
-#define data frame used for mapping
-defineMapPoints <- function(a)
-{
-
-#define points with only latitude, longitude, and league class
-points1 <- data.frame(x = as.numeric(tweets.running$lon), y = as.numeric(tweets.running$lat), 
-                      league = as.numeric(tweets.running$league))
-
-#ensure only points above mexico are considered
-points1 <- points1[points1$y > 25, ]
-
-return (points1)
-}
-
-mapData <- function(){
+mapData <- function(a){
   
   #US map
   map.data <- map_data("state")
@@ -129,9 +298,10 @@ mapData <- function(){
     expand_limits(x = map.data$long, y = map.data$lat) + 
     theme(axis.line = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(), 
           axis.title = element_blank(), panel.background = element_blank(), panel.border = element_blank(), 
-          panel.grid.major = element_blank(), plot.background = element_blank(), 
+          panel.grid.major = element_blank(), plot.background = element_blank(), legend.title=element_blank(),
           plot.margin = unit(0 * c(-1.5, -1.5, -1.5, -1.5), "lines")) + 
-    geom_point(data = points1, aes(x = x, y = y, color = factor(points1$league)), size = 2 , alpha = 1/2) +
+    geom_point(data = points, aes(x = x, y = y, color = factor(points$league)), size = 2 , alpha = 1/2) +
+    scale_colour_manual(values=cbbPalette) + 
     coord_cartesian(xlim = c(-60, -130))  #trim off unused edges on x axis, not lways needed
   
   return (p)
@@ -140,45 +310,42 @@ mapData <- function(){
 
 #print total counts of each league
 printTotals <- function()
-{
+{  
   #print totals
-  print(paste0("Total MLB Tweets: ", sum(tweets.running$league==1, na.rm=TRUE)))
-  print(paste0("Total NBA Tweets: ", sum(tweets.running$league==2, na.rm=TRUE)))
+  print(paste0("Total MLB Tweets: ", sum(tweets.running$league=="MLB", na.rm=TRUE)))
+  print(paste0("Total NBA Tweets: ", sum(tweets.running$league=="NBA", na.rm=TRUE)))
+  print(paste0("Total NFL Tweets: ", sum(tweets.running$league=="NFL", na.rm=TRUE)))
+  print(paste0("Total NHL Tweets: ", sum(tweets.running$league=="NHL", na.rm=TRUE)))
 }
 
+
+#run these prior to starting loop
+#handshake for twitter credentials
+my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+
+#define globally, used in many functions
+tweets.running <- data.frame(0)
+points <- data.frame(0)
+
+#Gather new tweets and update filter for first time
+tweets.running <- updateTweets(tweets.running, TRUE)
+
 #determine good length
-for (i in 1:3)
+for (i in 1:ITERATIONS)
 {
 
   #Gather new tweets and update filter
-  tweets.running <- updateTweets(tweets.running)
+  tweets.running <- updateTweets(tweets.running, FALSE)
   
-  #Prepare tweets to be mapped
-  points1 <- defineMapPoints(tweets.running)
+  #define points with only latitude, longitude, and league class
+  #update global so it can be used in ggplot
+  points <- data.frame(x = as.numeric(tweets.running$lon), y = as.numeric(tweets.running$lat), 
+                           league = tweets.running$league)
   
   #Define map and print it
-  map <- mapData()
+  map <- mapData(points)
   print(map)
   
   #Print total counts
   printTotals()
 }
-
-
-#remaining is UNUSED for capstone, only used as reference
-#all tweets map
-map.data <- map_data("state")
-
-#make data frame to represent coordinates for each tweet
-points <- data.frame(x = as.numeric(tweets.df$lon), y = as.numeric(tweets.df$lat))
-
-#remove points if latitude isn't greater than 25
-points <- points[points$y > 25, ]
-ggplot(map.data) + geom_map(aes(map_id = region), map = map.data, fill = "white", 
-                            color = "grey20", size = 0.25) + expand_limits(x = map.data$long, y = map.data$lat) + 
-  theme(axis.line = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(), 
-        axis.title = element_blank(), panel.background = element_blank(), panel.border = element_blank(), 
-        panel.grid.major = element_blank(), plot.background = element_blank(), 
-        plot.margin = unit(0 * c(-1.5, -1.5, -1.5, -1.5), "lines")) + geom_point(data = points, 
-                                                                                 aes(x = x, y = y), size = 1, alpha = 1/5, color = "darkblue") + 
-  coord_cartesian(xlim = c(-60, -130)) #Make map trim off unused edges on x axis
